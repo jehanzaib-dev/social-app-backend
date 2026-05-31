2c5c4b3196bc5633b8f0d35a24a51e43412a8097
Jehanzaib <jehanzaib.dev@gmail.com>
2026-05-31 08:10:46 +0500

    refactor: move multer to seperate file

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,20 +1,8 @@
-import express from "express";
-import multer from "multer";
+import Router from "express";
+import {upload} from '../middleware/upload.js';
 import {uploadImage} from '../controllers/uploadController.js';
 
-const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
+const router = Router();
 
 router.post("/", upload.single("file"), uploadImage);
 
