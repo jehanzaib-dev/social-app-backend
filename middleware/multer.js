0ff699c5ae44f58d697f4e7fa1f681cca7cac290
Jehanzaib <jehanzaib.dev@gmail.com>
2026-05-31 11:47:03 +0500

    cleanup: extra space cleanup

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,14 +1,15 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+    destination: (req, file, cb) => {
     cb(null, "public/images");
-  },
-
-  filename: (req, file, cb) => {
+       }, 
+    filename: (req, file, cb) => 
+      {
     cb(null, Date.now() + file.originalname);
-  },
-});
+      },
+  }
+);
 
 const upload = multer({ storage });
 
